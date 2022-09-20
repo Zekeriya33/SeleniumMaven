f3e64a9cbcfdf751cbf9a60752532d28590d3aa1
Zekeriya Bozkurt <bozkurt.zekeriya@gmail.com>
2022-09-20 09:55:27 +0300

    20/09/2022

--- src/test/java/day07/C04_Odev.java
+++ src/test/java/day07/C04_Odev.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
+import java.util.List;
 
 public class C04_Odev {
     /*
@@ -43,18 +44,28 @@ False yazdırın.
      //   driver.close();
     }
     @Test
-    public void test1() {
+    public void test1() throws InterruptedException {
   //   https://the-internet.herokuapp.com/dropdown adresine gidin.
         driver.get("https://the-internet.herokuapp.com/dropdown");
   //   Index kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
         WebElement ddm = driver.findElement(By.xpath("//*[@id='dropdown']"));
         Select select = new Select(ddm);
         select.selectByIndex(1);
+        Thread.sleep(1000);
   //   Value kullanarak Seçenek 2'yi (Option 2) seçin ve yazdırın
+        select.selectByValue("2");
+        Thread.sleep(1000);
   //   Visible Text(Görünen metin) kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
-  //   Tüm dropdown değerleri(value) yazdırın
-  //   Dropdown’un boyutunu bulun, Dropdown’da 4 öğe varsa konsolda True , degilse
+        select.selectByVisibleText("Option 1");
+   //   Tüm dropdown değerleri(value) yazdırın
+        System.out.println(ddm.getText());
+        //   Dropdown’un boyutunu bulun, Dropdown’da 4 öğe varsa konsolda True , degilse
   //   False yazdırın.
+        List<WebElement> drop =select.getOptions();
+        if (drop.size()==4){
+            System.out.println("True");
+        } else System.out.println("False");
+
 
     }
 }
