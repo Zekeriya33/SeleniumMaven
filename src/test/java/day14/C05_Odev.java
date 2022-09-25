a219015d23fc1a56f8ddaafb98b27c6b8685c56b
Zekeriya Bozkurt <bozkurt.zekeriya@gmail.com>
2022-09-25 20:12:06 +0300

    25/09/2022

--- src/test/java/day14/C05_Odev.java
+++ src/test/java/day14/C05_Odev.java
@@ -1,9 +1,28 @@
 package day14;
 
-public class C05_Odev {
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Test;
 
-    //-Dosya yolunu bir String degiskene atayalim
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class C05_Odev {
+    @Test
+    public void readExcelTest01() throws IOException {
+
+
+    //-Dosya yolunu bir String degiskene atayalimString
+        String dosyaYolu = "src/resources/ulkeler.xlsx";
     //-FileInputStream objesi olusturup,parametre olarak dosya yolunu girelim
+        FileInputStream fis = new FileInputStream(dosyaYolu);
     //-Workbook objesi olusturalim,parameter olarak fileInputStream objesini girelim
-    //-WorkbookFactory.create(fileInputStream)
+     //-WorkbookFactory.create(fileInputStream)
+        Workbook workbook = WorkbookFactory.create(fis);
+     //  //-sayfa 2'ye gidip satir sayisinin 6, kullanilan stun sayisinin ise 3 oldugunu test edin
+        String istenen = workbook.getSheet("Sayfa2").getRow(6).getCell(3).toString();
+        System.out.println(istenen);
+
+}
 }
